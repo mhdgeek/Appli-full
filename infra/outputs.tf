24ddcfcea1a123a1bddd658cfe3563fbdcfb124d
Mohamed <105009551+mhdgeek@users.noreply.github.com>
2025-10-31 09:32:42 +0000

    Add output for application server public IPs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,7 @@ output "rds_endpoint" {
   description = "Endpoint de la base de données RDS"
   value       = aws_db_instance.db.endpoint
 }
+
+output "app_servers" {
+  value = aws_instance.app[*].public_ip
+}
